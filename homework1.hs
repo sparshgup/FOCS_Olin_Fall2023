f1e9b9bf77ea0c5d91ae22eb51368a40bcfb1495
Sparsh Gupta <sparshg@gmail.com>
2023-09-14 11:50:29 -0400

    HW1 starter file

---
+++ homework1.hs
@@ -0,0 +1,117 @@
+{-
+
+--------------------------------------------------
+
+HOMEWORK 1
+
+Due: Thu, Sep 21, 2023 (23h59)
+
+Name:
+
+Email:
+
+Remarks, if any:
+
+
+
+--------------------------------------------------
+
+Please fill in this file with your solutions and submit it
+
+The functions below are stubs that you should replace with your
+own implementation.
+
+PLEASE DO NOT CHANGE THE TYPES IN THE STUBS BELOW.
+Doing so risks making it impossible for me to test your code.
+
+Always make sure you can :load this file in a FRESH ghci shell
+before submitting it. It has to load without any errors.
+
+--------------------------------------------------
+
+-}
+
+
+
+-- QUESTION 1
+
+clamp :: Float -> Float -> Float -> Float
+clamp a b c =
+  error "Not implemented"
+
+
+interpolate :: Float -> Float -> Float -> Float
+interpolate a b c =
+  error "Not implemented"
+
+
+-- QUESTION 2
+
+
+pairDouble :: [Int] -> [(Int, Int)]
+pairDouble xs =
+  error "Not implemented"
+
+
+cap :: Int -> [Int] -> [Int]
+cap m xs =
+  error "Not implemented"
+
+
+prefix :: String -> [String] -> [String]
+prefix s xs =
+  error "Not implemented"
+
+
+longerThan :: [String] -> [String]
+longerThan xs =
+  error "Not implemented"
+
+
+within :: Int -> Int -> [Int] -> [Int]
+within a b xs =
+  error "Not implemented"
+
+
+split :: Int -> [Int] -> ([Int], [Int])
+split s xs =
+  error "Not implemented"
+
+
+find :: Int -> [(Int, a)] -> a -> a
+find n xs def =
+  error "Not implemented"
+
+
+-- QUESTION 3
+
+vScale :: Float -> [Float] -> [Float]
+vScale a v =
+  error "Not implemented"
+
+
+vAdd :: [Float] -> [Float] -> [Float]
+vAdd v w =
+  error "Not implemented"
+
+
+vInner :: [Float] -> [Float] -> Float
+vInner v w =
+  error "Not implemented"
+
+
+vLength :: [Float] -> Float
+vLength v =
+  error "Not implemented"
+
+
+-- QUESTION 4
+
+repeatStr :: String -> Int -> String
+repeatStr s n =
+  error "Not implemented"
+
+
+collatzSeq :: Int -> [Int]
+collatzSeq n =
+  error "Not implemented"
